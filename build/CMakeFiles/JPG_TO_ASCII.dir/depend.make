42f0d0ea9e80bc6d5a51e0b98221ede71e382a6e
alyssaditroia <aditroia@asu.edu>
2025-01-18 21:23:23 -0700

    Add build folder to .gitignore

--- build/CMakeFiles/JPG_TO_ASCII.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for JPG_TO_ASCII.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/JPG_TO_ASCII.dir/src/UI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/JPG_TO_ASCII.dir/src/UI.cpp.o: \
-  /Users/alyssaditroia/Desktop/JPG_to_ASCII/src/UI.cpp \
-  /Users/alyssaditroia/Desktop/JPG_to_ASCII/include/UI.h
